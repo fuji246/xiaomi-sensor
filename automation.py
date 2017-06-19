0d99f9aee3ecb69bdaee6848a0cec50c2edaf0ab
Jeromy Fu <jianfu@cisco.com>
2017-06-19 00:26:58 -0700

    sensor code refactor and add automation example

---
+++ automation.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+from sensor import *
+import settings
+import requests
+import datetime
+
+def IsBetweenTime(start_time, end_time):
+    toMinutes = lambda h, m: 60 * int(h) + int(m)
+    now = datetime.datetime.now()
+    current = toMinutes(now.hour, now.minute)
+    start_minute = toMinutes(*(start_time.split(':')))
+    end_minute = toMinutes(*(end_time.split(':')))
+    if end_minute < start_minute:
+        end_minute += toMinutes(24, 0)
+    if current < start_minute:
+        current += toMinutes(24, 0)
+    return current <= end_minute
+
+def validBetweenTime(start_time, end_time):
+    def decoratorFunction(func):
+        def wrapper(*args, **kwargs):
+            if IsBetweenTime(start_time, end_time):
+                func(*args, **kwargs)
+        return wrapper
+    return decoratorFunction
+
+class IftttMixin(object):
+    def _trigger(self, url):
+        try:
+            r = requests.post(url)
+            r.raise_for_status()
+            logger.info('%s [%s]' % (url, r))
+            return True
+        except requests.exceptions.RequestException as e:
+            logger.error('trigger %s error: %s' % (url, e))
+            return False
+
+
+class OnOffControl(IftttMixin):
+
+    def __init__(self, on_url, off_url):
+        self.on = False
+        self.on_url = on_url
+        self.off_url = off_url
+
+    def turnOn(self):
+        if not self.on:
+            self._trigger(self.on_url)
+            self.on = not self.on
+
+    def turnOff(self):
+        if self.on:
+            self._trigger(self.off_url)
+            self.on = not self.on
+
+
+class LightControl(OnOffControl):
+
+    validTime = validBetweenTime(settings.LIVING_ROOM_LIGHT_START_TIME, settings.LIVING_ROOM_LIGHT_END_TIME)
+    
+    def __init__(self, on_url, off_url, toggle_url):
+        super(LightControl, self).__init__(on_url, off_url)
+        self.toggle_url = toggle_url
+
+    def onTimer(self):
+        if not IsBetweenTime(settings.LIVING_ROOM_LIGHT_START_TIME, settings.LIVING_ROOM_LIGHT_END_TIME):
+            self.turnOff()
+
+    def toggle(self):
+        self._trigger(self.toggle_url)
+        self.on = not self.on
+
+    @validTime
+    def turnOnInValidTime(self):
+        self.turnOn()
+
+    @validTime
+    def turnOffInValidTime(self):
+        self.turnOff()
+
+
+class HumidifierControl(OnOffControl):
+    pass
+
+
+class FanControl(OnOffControl):
+    pass
+
+
+class PurifyControl(OnOffControl):
+    pass
+
+
+class Automation(object):
+
+    def __init__(self):
+        self.light = LightControl(
+            settings.TURN_ON_LIVING_ROOM_LIGHT_URL,
+            settings.TURN_OFF_LIVING_ROOM_LIGHT_URL,
+            settings.TOOGLE_LIVING_ROOM_LIGHT_URL
+        )
+        self.light.turnOff()
+
+        self.humidifier = HumidifierControl(
+            settings.TURN_ON_BEDROOM_HUMIDIFIER,
+            settings.TURN_OFF_BEDROOM_HUMIDIFIER
+        )
+
+        self.fan = FanControl(
+            settings.TURN_ON_BEDROOM_FAN,
+            settings.TURN_OFF_BEDROOM_FAN
+        )
+
+        self.purify = PurifyControl(
+            settings.TURN_ON_BEDROOM_PURIFY,
+            settings.TURN_OFF_BEDROOM_PURIFY
+        )
+
+    def onTimer(self):
+        self.light.onTimer()
+
+    def onGatewayEvent(self, device):
+        logger.info('==> onGatewayEvent, %s' % device)
+    
+    def onSensorHtEvent(self, device):
+        logger.info('==> onSensorHtEvent, %s' % device)
+        if device.humidity <= settings.HUMIDITY_LOWER_THRSHOLD:
+            self.humidifier.turnOn()
+        elif device.humidity >= settings.HUMIDITY_UPPER_THRSHOLD:
+            self.humidifier.turnOff()
+
+        if device.temperature >= settings.TEMPERATURE_UPPER_THRSHOLD:
+            self.fan.turnOn()
+        elif device.temperature <= settings.TEMPERATURE_LOWER_THRSHOLD:
+            self.fan.turnOff()
+    
+    def onSwitchEvent(self, device):
+        logger.info('==> onSwitchEvent, %s' % device)
+        if device.status == 'click':
+            self.light.toggle()
+        elif device.status == 'double_click':
+            self.humidifier.turnOn()
+
+    def onMotionEvent(self, device):
+        logger.info('==> onMotionEvent, %s' % device)
+        if device.status == 'motion':
+            self.light.turnOnInValidTime()
+        elif device.status == 'no_motion':
+            self.light.turnOffInValidTime()
+
+
+if __name__ == '__main__':
+    automation = Automation() 
+    rules = {
+        'f0b429b442b1': automation.onGatewayEvent,
+        '158d000119f5c2': automation.onSensorHtEvent,
+        '158d00010def50': automation.onSwitchEvent,
+        '158d00010f3694': automation.onMotionEvent,
+    }
+
+    runLoop(rules, automation.onTimer)
--- sensor.py
+++ sensor.py
@@ -1,19 +1,59 @@
-from __future__ import print_function
-
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor, task
 import json
+import time
 
 MULTICAST_IP = '224.0.0.50'
 MULTICAST_PORT = 4321
 
+LOG_DIR = '/tmp/sensor.log'
+
+import logging
+logger = logging.getLogger('store')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+hdlr = logging.FileHandler(LOG_DIR)
+hdlr.setFormatter(formatter)
+hdlr.setLevel(logging.DEBUG)
+logger.addHandler(hdlr)
+
+console = logging.StreamHandler()
+console.setFormatter(formatter)
+console.setLevel(logging.INFO)
+logger.addHandler(console)
+
+
 def json_beauti(json_data):
     return json.dumps(json_data, indent=4)
+
+
+class Event(list):
+    def __call__(self, *args, **kwargs):
+        for f in self:
+            f(*args, **kwargs)
+
+    def __repr__(self):
+        return "Event(%s)" % list.__repr__(self)
+
 
 class Device(object):
 
     def __init__(self, device_id):
         self.device_id = device_id
+        self.battery = 100
+        self.event = Event()
+
+    def subscribe(self, callback):
+        logger.debug('%s subscribe %s' % (self.device_id, callback))
+        self.event.append(callback)
+
+    def unsubscribe(self, callback):
+        logger.debug('%s unsubscribe %s' % (self.device_id, callback))
+        self.event.remove(callback)
+
+    def onEvent(self):
+        self.event(self)
 
     def onHeartBeat(self, data):
         pass
@@ -25,7 +65,17 @@ class Device(object):
         pass
 
 
-class XMSensorHt(Device):
+class BatteryMixin(object):
+
+    MAX_VOLTAGE = 3300
+    LOW_VOLTAGE = 2800
+
+    def checkBattery(self, data):
+        voltage = data.get('voltage', self.MAX_VOLTAGE)
+        return 100 * (voltage - self.LOW_VOLTAGE) / (self.MAX_VOLTAGE - self.LOW_VOLTAGE)
+
+
+class XMSensorHt(Device, BatteryMixin):
 
     model = 'sensor_ht'
 
@@ -33,42 +83,44 @@ class XMSensorHt(Device):
         super(XMSensorHt, self).__init__(device_id)
         self.humidity = 0.0
         self.temperature = 0.0
-        self.battery = 100
 
     def onReadAck(self, data):
         self.humidity = int(data['humidity'])/100.0
         self.temperature = int(data['temperature'])/100.0
+        self.onEvent()
 
     def onReport(self, data):
         if 'humidity' in data:
             self.humidity = int(data['humidity'])/100.0
         if 'temperature' in data:
             self.temperature = int(data['temperature'])/100.0
-        self.battery = data.get('battery', self.battery)
+        self.battery = self.checkBattery(data)
+        self.onEvent()
 
     def __str__(self):
-        return '%s [%s]: humidity: %d%%, temperature: %d C' %\
-            (self.model, self.device_id, self.humidity, self.temperature)
+        return '%s [%s] %d%%: humidity: %d%%, temperature: %d C' %\
+            (self.model, self.device_id, self.battery, self.humidity, self.temperature)
 
 
-class XMSensorStatus(Device):
+class XMSensorStatus(Device, BatteryMixin):
 
     def __init__(self, device_id):
         super(XMSensorStatus, self).__init__(device_id)
-        self.battery = 100
         self.status = None
 
     def onReadAck(self, data):
         self.status = data.get('status', self.status)
-
-    def onReport(self, data):
-        self.battery = data.get('battery', self.battery)
+        self.onEvent()
 
     def onHeartBeat(self, data):
-        self.status = data['status']
+        self.battery = self.checkBattery(data)
+
+    def onReport(self, data):
+        self.status = data.get('status', self.status)
+        self.onEvent()
 
     def __str__(self):
-        return '%s [%s][%d%%]: %s' % (self.model, self.device_id, self.battery, self.status)
+        return '%s [%s] %d%%: %s' % (self.model, self.device_id, self.battery, self.status)
 
 
 class XMSensorMagnet(XMSensorStatus):
@@ -80,33 +132,28 @@ class XMSensorMotion(XMSensorStatus):
 
     model = 'motion'
 
-    def onReport(self, data):
-        super(XMSensorStatus, self).onReport(data)
-
 
 class XMSensorSwitch(XMSensorStatus):
 
     model = 'switch'
 
 
-SUB_DEVICE_MAP = {
-    XMSensorHt.model: XMSensorHt,
-    XMSensorMagnet.model: XMSensorMagnet,
-    XMSensorMotion.model: XMSensorMotion,
-    XMSensorSwitch.model: XMSensorSwitch,
-}
-
-
 class XMGateway(Device):
 
     model = 'gateway'
+    min_illumination = 300
+    max_illumination = 1300
 
-    def __init__(self, ip, port, transport, device_id):
+    def __init__(self, device_id):
         super(XMGateway, self).__init__(device_id)
+        self.device_list_ready = False
+        self.rgb = ''
+        self.illumination = 0
+
+    def setTransport(self, ip, port, transport):
         self.ip = ip
         self.port = port
         self.transport = transport
-        self.device_lst = []
 
     def getDevices(self):
         data = b'{"cmd":"get_id_list"}'
@@ -117,55 +164,87 @@ class XMGateway(Device):
         self.sendCmd(data)
 
     def sendCmd(self, data):
-        print('send cmd: %s to %s:%s' % (data, self.ip, self.port))
+        logger.debug('send cmd: %s to %s:%s' % (data, self.ip, self.port))
         self.transport.write(data, (self.ip, self.port))
 
-    def addDevices(self, device_lst):
-        self.device_lst = device_lst
-
-    def readDevices(self):
-        for device_id in self.device_lst:
+    def readDevices(self, device_lst):
+        for device_id in device_lst:
             self.readDevice(device_id)
 
+    def onGatewayLightData(self, data):
+        self.rgb = str(hex(data['rgb']))[4:]
+        self.illumination = int(data['illumination'])
+        self.onEvent()
+
+    def onReport(self, data):
+        self.onGatewayLightData(data)
+
+    def onReadAck(self, data):
+        self.onGatewayLightData(data)
+
+    def onDeviceList(self, data):
+        self.readDevice(self.device_id)
+        self.readDevices(data)
+        self.device_list_ready = True
+
     def __str__(self):
-        return '%s [%s] %s:%d' % (self.model, self.device_id, self.ip, self.port)
+        return '%s [%s] %s:%d, rgb: #%s, illumination: %d' % \
+            (self.model, self.device_id, self.ip, self.port, self.rgb, self.illumination)
+
+
+DEVICE_FACTORY_MAP = {
+    XMSensorHt.model: XMSensorHt,
+    XMSensorMagnet.model: XMSensorMagnet,
+    XMSensorMotion.model: XMSensorMotion,
+    XMSensorSwitch.model: XMSensorSwitch,
+    XMGateway.model: XMGateway,
+}
 
 
 class XMProtocol(DatagramProtocol):
 
-    def __init__(self):
+    def __init__(self, rules, timer_hook):
+        self.rules = rules
         self.gateway = {}
-        self.sub_device = {}
+        self.devices = {}
         self.lc = task.LoopingCall(self.onTimer)
+        self.timer_hook = timer_hook
 
     def onTimer(self):
         if len(self.gateway) == 0:
             self.searchGateway()
+        for gateway in self.gateway.values():
+            if not gateway.device_list_ready:
+                gateway.getDevices()
+        if self.timer_hook is not None:
+            self.timer_hook()
 
     def startProtocol(self):
         self.transport.joinGroup(MULTICAST_IP)
         self.searchGateway()
-        self.lc.start(1)
+        self.lc.start(3, False)
 
     def searchGateway(self):
         data = b'{"cmd":"whois"}'
-        print('finding gateway: %s ...' % data)
+        logger.info('finding gateway: %s ...' % data)
         self.transport.write(data, (MULTICAST_IP, MULTICAST_PORT))
 
-    def getSubDevice(self, device_id, model):
-        if device_id in self.sub_device:
-            if self.sub_device[device_id].model == model:
-                return self.sub_device[device_id]
+    def getOrCreateDevice(self, device_id, model):
+        if device_id in self.devices:
+            if self.devices[device_id].model == model:
+                return self.devices[device_id]
             else:
-                print('model of %s not match, %s, %s' % device_id, sub_device.model, model)
+                logger.warn('model of %s not match, %s, %s' % device_id, self.devices[device_id].model, model)
         else:
-            if model in SUB_DEVICE_MAP:
-                model_class = SUB_DEVICE_MAP[model]
-                sub_device = model_class(device_id)
-                self.sub_device[device_id] = sub_device
-                return sub_device
+            if model in DEVICE_FACTORY_MAP:
+                model_class = DEVICE_FACTORY_MAP[model]
+                device = model_class(device_id)
+                self.devices[device_id] = device
+                if device_id in self.rules:
+                    device.subscribe(self.rules[device_id])
+                return device
             else:
-                print('model not found: %s' % model)
+                logger.warn('model not found: %s' % model)
                 return None
 
     def datagramReceived(self, data, addr):
@@ -173,49 +252,45 @@ class XMProtocol(DatagramProtocol):
             replace('"{', '{').replace('}"', '}').\
             replace('\\"','"')
         json_data = json.loads(data)
-        print('\n=====>> received:\n%s\n' % json_beauti(json_data))
+        logger.debug('\n=====>> received:\n%s\n' % json_beauti(json_data))
 
         self.parseCmdData(json_data)
 
     def onReadAck(self, json_data):
-        sub_device = self.getSubDevice(json_data['sid'], json_data['model'])
-        if sub_device:
-            sub_device.onReadAck(json_data['data'])
+        device = self.getOrCreateDevice(json_data['sid'], json_data['model'])
+        if device:
+            device.onReadAck(json_data['data'])
+            logger.info('onReadAck, device: %s' % device)
 
     def onHeartBeat(self, json_data):
-        device_id = json_data['sid']
-        device_model = json_data['model']
-        if device_id in self.gateway:
-            self.gateway[device_id].onHeartBeat(json_data['data'])
-        else:
-            sub_device = self.getSubDevice(device_id, device_model)
-            if sub_device:
-                sub_device.onHeartBeat(json_data['data'])
+        device = self.getOrCreateDevice(json_data['sid'], json_data['model'])
+        if device:
+            device.onHeartBeat(json_data['data'])
 
     def onReport(self, json_data):
-        sub_device = self.getSubDevice(json_data['sid'], json_data['model'])
-        if sub_device:
-            sub_device.onReport(json_data['data'])
+        device = self.getOrCreateDevice(json_data['sid'], json_data['model'])
+        if device:
+            device.onReport(json_data['data'])
 
     def parseCmdData(self, json_data):
         if 'data' in json_data and 'error' in json_data['data']:
-            print(json_data['data']['error'])
+            logger.error(json_data['data']['error'])
 
         cmd = json_data['cmd']
 
         if cmd == 'iam' and json_data['model'] == 'gateway':
             device_id = json_data['sid']
             if device_id not in self.gateway:
-                gateway = XMGateway(json_data['ip'], int(json_data['port']), self.transport, device_id)
+                gateway = self.getOrCreateDevice(json_data['sid'], json_data['model'])
+                gateway.setTransport(json_data['ip'], int(json_data['port']), self.transport)
                 self.gateway[device_id] = gateway
-                print('gateway found: %s' % gateway)
+                logger.info('gateway found: %s' % gateway)
                 gateway.getDevices()
         elif cmd == 'get_id_list_ack':
             device_id = json_data['sid']
             if device_id in self.gateway:
                 gateway = self.gateway[device_id]
-                gateway.addDevices(json_data['data'])
-                gateway.readDevices()
+                gateway.onDeviceList(json_data['data'])
         elif cmd == 'read_ack':
             self.onReadAck(json_data)
         elif cmd == 'heartbeat':
@@ -224,6 +299,11 @@ class XMProtocol(DatagramProtocol):
             self.onReport(json_data)
 
 
-if __name__ == '__main__':
-    reactor.listenMulticast(9898, XMProtocol(), listenMultiple=True)
+def runLoop(rules={}, timer_hook=None):
+    proto = XMProtocol(rules, timer_hook)
+    reactor.listenMulticast(9898, proto, listenMultiple=True)
     reactor.run()
+
+if __name__ == '__main__':
+    runLoop()
+
